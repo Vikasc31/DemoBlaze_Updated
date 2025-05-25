b2e33caf9accca5b9987113e59d6b96d2f963b3e
Vikas Chavan <vchavan@verticalfox.com>
2025-05-26 00:24:46 +0530

    add report genrations

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,14 +6,18 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		
+
 		features = "C:\\Users\\VikasChavan\\eclipse-workspace\\Demoblaze1\\features\\home.feature",
-		
+
 		glue = "stepdef",
-		
+
+		plugin = { "pretty", "html:target/cucumber-reports.html",
+		        "json:target/cucumber.json",
+		        "junit:target/cucumber.xml"},
+
 		dryRun = false
-			
-		)
+
+)
 
 public class TestRunner {
 
